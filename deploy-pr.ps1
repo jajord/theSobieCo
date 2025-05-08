b3db68669f492bfe44bad61d116fecfd833943f1
i-eat-rocks <jjordan8@una.edu>
2025-05-08 10:26:04 -0500

    minor revision

--- deploy-pr.ps1
+++ deploy-pr.ps1
@@ -1,22 +1,10 @@
 param (
   [string]$User
 )
-$baseDir = "C:\Users\Tester\Desktop\PRs\$User"
-New-Item $baseDir -ItemType Directory
-Set-Location $baseDir
-
-$env:PORT = [int] $env:PORT + 1
-$port = $env:PORT
-
-Write-Host ""
-
-# Kill any existing process on that port
-Get-NetTCPConnection -LocalPort $port -ErrorAction SilentlyContinue | ForEach-Object {
-    Stop-Process -Id $_.OwningProcess -Force -ErrorAction SilentlyContinue
-}
+New-Item -Force -Path .. -Name "$User" -ItemType Directory
+Set-Location ..\$User
+Copy-Item -Recurse -Force -Path ..\temp\* -Destination .
 
 # Install dependencies and start the app
-# npm install
-
+Invoke-Expression "npm install"
 Invoke-Expression "pm2 start app.js --name $User"
-Write-Host "App should be running at http://$User.fridayoclock.com on (Port $port)"
