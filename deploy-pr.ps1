b43f1082b119f8aefc165acd9b0a4fd64305e0df
i-eat-rocks <jjordan8@una.edu>
2025-05-08 11:30:51 -0500

    minor revision

--- deploy-pr.ps1
+++ deploy-pr.ps1
@@ -1,10 +1,12 @@
 param (
   [string]$User
 )
+# Maximum verbosity to appease Bill Gates
 New-Item -Force -Path .. -Name "$User" -ItemType Directory
-Set-Location ..\$User
+Set-Location -Path ..\$User
 Copy-Item -Recurse -Force -Path ..\temp\* -Destination .
+Copy-Item -Force -Path "$env:dotenvPath\.env" -Destination .env
 
 # Install dependencies and start the app
-Invoke-Expression "npm install"
-Invoke-Expression "pm2 start app.js --name $User"
+Invoke-Expression -Command "npm install"
+Invoke-Expression -Command "pm2 start app.js --name $User"
